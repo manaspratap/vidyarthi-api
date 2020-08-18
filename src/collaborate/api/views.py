f87df731d1ad9b4b98333adef146977f21fbe70d
manas pratap thakur <manasp@geekyants.com>
2020-08-19 01:17:35 +0530

    ml integrated part(1/2)

--- src/collaborate/api/views.py
+++ src/collaborate/api/views.py
@@ -26,9 +26,67 @@ def collaborate_view(request):
     data = {}
     if request.method == 'POST':
         searchWord = request.data.get('search_word',0)
-        print('-----------'+str(searchWord))
+        # print('-----------'+str(searchWord))
         # call a function here eg. data["page"] = nlpFunction (searchWord)
         # send either me, collaborate, course, support
         # data["page"]='collaborate'
         # return Response(data=data)
+        projectCollaborate(searchWord)
         return Response(SUCCESS)
+
+import pandas as pd
+from pandas import DataFrame
+
+def adjust_compat_team_attributes(attribute_name, user, collaborator):
+    if user[attribute_name] == 0 or collaborator[attribute_name[5:]] == 0:
+        return 0
+    return abs(user[attribute_name] - collaborator[attribute_name[5:]]) * -5
+        
+def adjust_compat_standalone_attributes(attribute_name, user, collaborator):
+    if user[attribute_name] == 0 or collaborator[attribute_name] == 0:
+        return 0
+    return abs(user[attribute_name] - collaborator[attribute_name]) * -5
+
+    
+def projectCollaborate(searchWord):    
+    projectprofiles = pd.read_csv('./user-project-profiles.csv')
+
+    #Inputs - Come From FrontEnd of Application
+    user_id = 10 # int(input("User ID : "))
+    project_track = searchWord
+
+    matching_track_profiles = DataFrame(columns=projectprofiles.columns)
+    k = 1
+
+    for index, row in projectprofiles.iterrows():
+        if row['User_ID'] == user_id:
+            user_profile = row
+            continue
+        tracks = row['Tracks_Worked_On'].split('.')
+        if project_track in tracks:
+            matching_track_profiles.loc[k] = row
+            k+=1
+        
+    matching_track_profiles["Compatibility_Score"] = 100
+
+    compat_attributes = ['Team_Work_Time_Preference', 'Alignment_of_Work', 'Team_Learn_and_Work_Preference', 'Team_Working_Hours_Preference', 'Mode_of_Communication_Preference', 'Style_of_Communication_Preference']
+
+
+    for index, row in matching_track_profiles.iterrows():
+        compat_adjust = 0
+        for attribute in compat_attributes:
+            if attribute[0:4] == 'Team':
+                compat_adjust += adjust_compat_team_attributes(attribute, user_profile, row)
+            else:
+                compat_adjust += adjust_compat_standalone_attributes(attribute, user_profile, row)
+        row["Compatibility_Score"] = row["Compatibility_Score"] + compat_adjust
+        matching_track_profiles.loc[index] = row
+
+    matching_profiles_dict = {}
+
+    for index, row in matching_track_profiles.iterrows():
+        matching_profiles_dict[row["User_ID"]] = row["Compatibility_Score"]
+    
+    # return here
+    print('-------------321'+str(matching_profiles_dict))
+    matching_track_profiles
--- src/course/api/views.py
+++ src/course/api/views.py
@@ -26,9 +26,63 @@ def course_view(request):
     data = {}
     if request.method == 'POST':
         searchWord = request.data.get('search_word',0)
-        print('-----------2'+str(searchWord))
+        # print('-----------2'+str(searchWord))
         # call a function here eg. data["page"] = nlpFunction (searchWord)
         # send either me, collaborate, course, support
         # data["page"]='collaborate'
         # return Response(data=data)
+        recommendCourse(searchWord)
         return Response(SUCCESS)
+
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+def feature_combination(row):
+    return row["course_title"] + " " + row["platform"] + " " + str(row["course_index"]) + " " + str(row["difficulty"])
+
+def tokenize_remove_stopwords(sentence, _stopwords):
+    word_tokens = word_tokenize(sentence)
+    word_tokens_cleaned = {word for word in word_tokens if word not in _stopwords}
+    return ' '.join(list(word_tokens_cleaned))
+
+def recommendCourse(searchWord):
+
+    dataset = pd.read_csv("all-course-data.csv")
+
+    dataset["difficulty"] = dataset["level"].map({"Advanced":100, "advanced":90, "All Levels":50, "Mixed":60, "Expert Level":80, "Intermediate": 40, "intermediate ":40, "intermediate":40, "Intermediate Level":45, "beginner":10, "Beginner":20, "Beginner Level":30})
+    dataset.drop("level", axis=1, inplace=True)
+
+    features = ["course_title", "course_index", "difficulty", "platform"]
+
+    dataset["cumulative_features"] = dataset.apply(feature_combination, axis = 1)
+
+    cv = CountVectorizer()
+    _stopwords = stopwords.words('english')
+
+    user_preference = searchWord
+    user_difficulty = 40
+
+    user_preference_string = tokenize_remove_stopwords(user_preference, _stopwords).title()
+
+    required_data = dataset[features]
+    required_data.loc[len(required_data)] = [user_preference_string, len(required_data), user_difficulty, "None"] 
+    word_bag = cv.fit_transform(required_data["course_title"])
+    word_list = word_bag.toarray()
+    csim = cosine_similarity(word_list)
+
+    similar_courses = list(enumerate(csim[len(required_data) - 1]))
+    sorted_similar_courses = sorted(similar_courses,key=lambda x:x[1],reverse=True)[1:50]
+    i=0
+    recommended_courses = dict()
+    for element in sorted_similar_courses:
+        course = required_data.loc[element[0]]
+        if(course["difficulty"] >= user_difficulty - 15 and course["difficulty"] <= user_difficulty + 15 and element[0] != len(required_data) - 1):
+            recommended_courses[course["course_index"], course["platform"]] = 100 - abs(user_difficulty - course["difficulty"])
+        
+    # integrate
+    print('------------------12------'+str(recommended_courses))
+
